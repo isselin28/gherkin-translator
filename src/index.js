f771071057e00d8ac9ab845f4fc55faa12f7aac0
isselin28 <isselin.moektijono@gojek.com>
2022-11-18 02:48:43 +0800

    add routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export default function BaseApp() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/gherkin-translator" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BaseApp />
   </React.StrictMode>
 );
 
